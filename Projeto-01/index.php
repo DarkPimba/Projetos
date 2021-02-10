38daa6d61ce5c9d5d5dda7fc2bed2b3ffa70a9ab
Carlos Alberto <juniorjp43215@gmail.com>
2021-02-10 11:05:30 -0300

    update

---
+++ Projeto-01/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras,do,meu,site">
+    <meta name="description" content="descrição do meu site">
+    <title>Projeto-01</title>
+</head>
+<body>
+
+    <header>
+    
+        <div class="logo">Logomarca</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--mobile-->
+
+    </header>
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" require />
+            <input type="submit" name="acao" value="Cadastrar" />
+        </form>
+    </section><!--banner-principal-->
+    
+</body>
+</html>
