f7c8b9cfe927c31431c690bdb951a6413f3449a4
Carlos Alberto <juniorjp43215@gmail.com>
2021-02-12 05:53:21 -0300

    update

--- Projeto-01/index.php
+++ Projeto-01/index.php
@@ -1,10 +1,11 @@
+<?php include('config.php'); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7d4ffebbc2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>style/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,116 +17,44 @@
 
     <header>
     <div class="center">
-        <div class="logo left"><a href="">Logomarca</a></div><!--logo-->
+        <div class="logo left"><a href="<?php echo INCLUDE_PATH ?>">Logomarca</a></div><!--logo-->
         <nav class="desktop right">
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>">Home</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>sobre">Sobre</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>servicos">Serviços</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>contato">Contato</a></li>
             </ul>
         </nav><!--desktop-->
         <nav class="mobile right">
             <div class="botao-menu-mobile"><i class="fas fa-bars"></i></div>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>">Home</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>sobre">Sobre</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>servicos">Serviços</a></li>
+                <li><a href="<?php echo INCLUDE_PATH ?>contato">Contato</a></li>
             </ul>
         </nav><!--mobile-->
     </div><!--center-->
     <div class="clear"></div>
     </header>
 
-    <section class="banner-principal">
-        <div class="overlay"></div><!--overlay-->
-        <div class="center">
-            <form>
-                <h2>Qual o seu melhor e-mail?</h2>
-                <input type="email" name="email" require />
-                <input type="submit" name="acao" value="Cadastrar" />
-            </form>
-        </div><!--center-->
-    </section><!--banner-principal-->
-    <div class="center">
-        <section class="descricao-autor">
-        <div class="w50 left">
-            <h2>Carlos A. Silva</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget erat egestas, lobortis ante et, interdum metus. Nulla eu iaculis nulla, sit amet lacinia diam. Cras porttitor justo sit amet ex ullamcorper, ac consequat risus tempus. Curabitur quis leo lacus. Integer lacinia sem nisi, scelerisque laoreet quam fringilla quis. Phasellus sodales orci eget diam aliquam, vel scelerisque magna aliquet.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget erat egestas, lobortis ante et, interdum metus. Nulla eu iaculis nulla, sit amet lacinia diam. Cras porttitor justo sit amet ex ullamcorper, ac consequat risus tempus. Curabitur quis leo lacus. Integer lacinia sem nisi, scelerisque laoreet quam fringilla quis. Phasellus sodales orci eget diam aliquam, vel scelerisque magna aliquet.</p>
-        </div><!--w50-->
-        <div class="w50 left">
-            <img class="img right" src="img/ft.jpg" />
-        </div><!--w50-->
-        <div class="clear"></div>
-    </div><!--center-->
-    </section><!--descricao-autor-->
-
-    <section class="especialidades">
+    <?php 
+    
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
         
-        <div class="center">
-            <h2 class="title">Especialidades</h2>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-css3"></i></h3>
-                <h4>CSS3</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis,</p>
-            </div><!--box-especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-html5"></i></h3>
-                <h4>HTML5</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis,</p>
-            </div><!--box-especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-js"></i></h3>
-                <h4>JavaScript</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis,</p>
-            </div><!--box-especialidade-->
-        </div><!--center-->
-        <div class="clear"></div>
-    </section><!--especialidades-->
-
-    <section class="extras">
+        if(file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }
+        else{
+            //podemos fazer o que quiser pois a página não existe.
+            include('pages/404.php');
+        }
     
-        <div class="center">
-            <div class="w50 left depoimentos-container">
-                <h2 class="title">Depoimentos dos nosso clientes</h2>
-                <div class="depoimento-sigle">
-                <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</p>
-                <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-sigle-->
-                <div class="depoimento-sigle">
-                <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</p>
-                <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-sigle-->
-                <div class="depoimento-sigle">
-                <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</p>
-                <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-sigle-->
-            </div><!--w50-->
-            <div class="w50 left servicos-container">
-                <h2 class="title">Serviços</h2>
-                <div class="servicos">
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium rutrum ullamcorper. Etiam sem lectus, condimentum eget rhoncus vel, pellentesque nec sapien. Pellentesque placerat eu erat nec consectetur. Pellentesque in sem vel urna tristique lacinia sit amet eget nunc. Integer blandit odio quis ultricies elementum. Suspendisse auctor libero turpis, at placerat est gravida in. Fusce at gravida odio, ut faucibus orci. Vivamus eget</li>
-                    </ul>
-                </div><!--serviços-->
-            </div><!--w50-->
-            
-        </div><!--center-->
-            <div class="clear"></div>
-    </section><!--extras-->
-
-    <footer>
-    
-        <div class="center">
-            <p>Todos os direitos reservados</p>
-        </div><!--center-->
+    ?>
 
     </footer>
-    <script src="js/jquery.js"></script>
-    <script src="js/scripts.js"></script>
+    <script src="<?php echo INCLUDE_PATH ?>js/jquery.js"></script>
+    <script src="<?php echo INCLUDE_PATH ?>js/scripts.js"></script>
 </body>
 </html>
--- Projeto-01/js/scripts.js
+++ Projeto-01/js/scripts.js
@@ -1,13 +1,24 @@
 $(function(){
     $('nav.mobile').click(function(){
         var listaMenu = $('nav.mobile ul');
+        var icone = $('.botao-menu-mobile').find('i')
+
         //abrir menu mobile pedo fadeIn
-        /*if(listaMenu.is(':hidden') == true)
-            listaMenu.fadeIn();
-            else
-                listaMenu.fadeOut();
-        */
+        
+        if(listaMenu.is(':hidden') == true){
+            icone.removeClass('fa-bars');
+            icone.addClass('fa-times');
+            listaMenu.slideToggle();
+        }
+        else{
+            icone.removeClass('fa-times');
+            icone.addClass('fa-bars');
+            listaMenu.slideToggle();
+        }
+        
        //abre e fecha o menu mobile
-       listaMenu.slideToggle();
+       
+     
+     //<i class="fas fa-times"></i>
     })
 })
