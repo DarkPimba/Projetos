ec57eccf0aaa0e6a1fe6c1e9c071b7591c0105bf
Carlos Alberto <juniorjp43215@gmail.com>
2021-02-12 03:53:20 -0300

    update

---
+++ Projeto-01/js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('nav.mobile').click(function(){
+        var listaMenu = $('nav.mobile ul');
+        //abrir menu mobile pedo fadeIn
+        /*if(listaMenu.is(':hidden') == true)
+            listaMenu.fadeIn();
+            else
+                listaMenu.fadeOut();
+        */
+       
+    })
+})
